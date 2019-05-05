c2fe80ff7e7e51a9f584b8aed6e87660ec8701f8
wuijahai168@126.com <wuijahai168@126.com>
2019-05-05 17:10:02 +0800

    add 5

--- GobangTest/assets/Script/Game.js
+++ GobangTest/assets/Script/Game.js
@@ -74,7 +74,7 @@ cc.Class({
                 newNode.on(cc.Node.EventType.TOUCH_END,function(event){
                     self.touchChess = this;
                     if( self.gameState=="black" && this.getComponent(cc.Sprite).SpriteFrame===null ){
-                        this.getComponent( cc.Sprite ).SpriteFrame = self.blackSpriteFrame;
+                        this.getComponent( cc.Sprite ).spriteFrame = self.blackSpriteFrame;
                         self.judgeOver();
                         if( self.gameState=="white" ){
                             self.scheduleOnce(function(){ self.ai(); },1);
@@ -82,12 +82,58 @@ cc.Class({
                     }
                 },this);
 
+                this.chessList.push(newNode);
+
                 //newNode.setTag = y*15+x;
-                //newNode.tag = y*15+x;//根据每个节点的tag就可以算出其二维坐标
+                newNode._tag = y*15+x;//根据每个节点的tag就可以算出其二维坐标
                 //newNode.setTag( y*15 + x );
-                //cc.log(newNode);
+                cc.log(newNode);
             }
         }
+
+        //cc.log(this.chessList);
+
+        this.chessList[112].getComponent(cc.Sprite).spriteFrame = this.whiteSpriteFrame;
+        this.gameState = "black";
+
+        //cc.log( this.chessList[112] );
+
+        //横向
+        for( var y=0; y<15; y++ ){
+            for( var x=0; x<15; x++ ){
+                this.fiveGroup.push([y*15+x,y*15+x+1,y*15+x+2,y*15+x+3,y*15+x+4]);
+            }
+        }
+
+        //纵向
+        for( var x=0; x<15; x++ ){
+            for( var y=0; y<15; y++ ){
+                this.fiveGroup.push( [y*15+x, (y+1)*15+x, (y+2)*15+x, (y+3)*15+x, (y+4)*15+x ] );
+            }
+        }
+
+        //右上
+        for( var b=-10; b<=10; b++ ){
+            for( var x=0; x<11; x++ ){
+                if( b+x<0 || b+x>10 ){
+                    continue;
+                }else{
+                    this.fiveGroup.push( [(b+x)*15+x, (b+x+1)*15+x+1, (b+x+2)*15+2,(b+x+3)*15+3,(b+x+4)*15+4 ] );
+                }
+            }
+        }
+
+        //右下
+        for( var b=4; b<=24; b++ ){
+            for( var y=0; y<11; y++ ){
+                if( b-y<4 || b-y>14 ){
+                    continue;
+                }else{
+                    this.fiveGroup.push( [y*15+b-y, (y+1)*15+b-y-1, (y+2)*15+b-y-2, (y+3)*15+b-y-3, (y+4)*15+b-y-4 ] );
+                }
+            }
+        }
+
     },
 
     start () {
@@ -102,6 +148,192 @@ cc.Class({
 
     backMenu(){
         cc.director.loadScene("Menu");
+    },
+
+    //电脑下棋
+    ai(){
+
+        //评分
+        for( var i=0; i<this.fiveGroup.length; i++ ){
+            var b=0;
+            var w=0;
+            for( var j=0; j<5; j++ ){
+                //this.getComponent(cc.Sprite).spriteFrame;
+                if( this.chessList[this.fiveGroup[i][j]].getComponent(cc.Sprite).spriteFrame == this.blackSpriteFrame ){
+                    b++;
+                }else if( this.chessList[this.fiveGroup[i][j]].getComponent(cc.Sprite).spriteFrame == this.whiteSpriteFrame ){
+                    w++;
+                }
+            }
+
+            if( b+w == 0 ){
+                this.fiveGroupScore[i] = 7;
+            }else if( b>0 && w>0 ){
+                this.fiveGroupScore[i] = 0;
+            }else if( b==0 && w==1 ){
+                this.fiveGroupScore[i] = 35;
+            }else if( b==0 && w==2 ){
+                this.fiveGroupScore[i] = 800;
+            }else if( b==0 && w==3 ){
+                this.fiveGroupScore[i] = 1500;
+            }else if( b==0 && w==4 ){
+                this.fiveGroupScore[i] = 800000;
+            }else if( w==0 && w==1 ){
+                this.fiveGroupScore[i] = 15;
+            }else if( w==0 && w==2 ){
+                this.fiveGroupScore[i] = 400;
+            }else if( w==0 && w==3 ){
+                this.fiveGroupScore[i] = 1800;
+            }else if( w==0 && w==4 ){
+                this.fiveGroupScore[i] = 100000;
+            }
+
+        }
+
+        //最高分
+        var hScore = 0;
+        var mPosition = 0;
+        for( var i=0; i<this.fiveGroupScore.length; i++ ){
+            if( this.fiveGroupScore[i]>hScore ){
+                hScore = this.fiveGroupScore[i];
+                mPosition = (function(x){
+                    return x;
+                })(i);
+            }
+        }
+
+        var flag1 = false;  //无子
+        var flag2 = false;  //有子
+        var nPosition = 0;
+
+        for( var i=0; i<5; i++ ){
+            if( !flag1 && this.chessList[this.fiveGroup[mPosition]].getComponent(cc.SpriteFrame == null) ){
+                nPosition = (function(x){return x;})(i);
+            }
+            if( !flag2 && this.chessList[this.fiveGroup[mPosition]].getComponent(cc.SpriteFrame) != null ){
+                flag1 = true;
+                flag2 = true;
+            }
+            if( !flag2 && this.chessList[this.fiveGroup[mPosition]].getComponent(cc.SpriteFrame) == null ){
+                nPos = (function(x){return x;})(i);
+                break;
+            }
+        }
+
+        this.chessList[this.fiveGroup[mPosition][nPosition]].getComponent( cc.Sprite );
+        this.touchChess = this.chessList[his.fiveGroup[mPosition][nPosition]];
+        this.judgeOver();
+
+    },
+
+
+    //判断
+    judgeOver(){
+        var x0 = this.touchChess._tag % 15;
+        var y0 = parseInt( this.touchChess._tag / 15 );
+        
+        //判断横向
+        var fiveCount = 0;
+        for( var x=0; x<15; x++ ){
+            if( this.chessList[y0*15+x].getComponent(cc.Sprite).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame ){
+                fiveCount ++;
+                if( fiveCount == 5 ){
+                    if( this.gameState === "black" ){
+                        this.overLabel.string = "你赢了";
+                        this.overSprite.node.x = 0;
+                    }else{
+                        this.overLabel.string = "你输了";
+                        this.overSprite.node.x = 0;
+                    }
+                    this.gameState = "over";
+                    return ;
+                }
+            }else{
+                fiveCount = 0;
+            }
+        }
+
+        //判断纵向
+        fiveCount = 0;
+        for( var y=0; y<15; y++ ){
+            if( this.chessList[y*15+x0].getComponent(cc.Sprite).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame ){
+                fiveCount++;
+                if( fiveCount==5 ){
+                    if( fiveCount == 5 ){
+                        if( this.gameState === "black" ){
+                            this.overLabel.string = "你赢了";
+                            this.overSprite.node.x = 0;
+                        }else{
+                            this.overLabel.string = "你输了";
+                            this.overSprite.node.x = 0;
+                        }
+                        this.gameState = "over";
+                        return ;
+                    }
+                }else{
+                    fiveCount=0;
+                }
+            }
+        }
+
+        //判断右上
+        var f=y0-x0;
+        fiveCount=0;
+        for( var x=0; x<15; x++ ){
+            if( f+x<0 || f+x>14 ){
+                continue;
+            }
+            if( this.chessList[(f+x)*15+x].getComponent(cc.Sprite).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame ){
+                fiveCount++;
+                if( fiveCount==5 ){
+                    if( fiveCount == 5 ){
+                        if( this.gameState === "black" ){
+                            this.overLabel.string = "你赢了";
+                            this.overSprite.node.x = 0;
+                        }else{
+                            this.overLabel.string = "你输了";
+                            this.overSprite.node.x = 0;
+                        }
+                        this.gameState = "over";
+                        return ;
+                    }
+                }else{
+                    fiveCount=0;
+                }
+            }
+        }
+
+        //判断右下
+        f = y0+x0;
+        fiveCount=0;
+        for(var x=0; x<15; x++){
+            if( f-x<0 || f-x>14 ){
+                continue;
+            }
+            if( this.chessList[(f-x)*15+x].getComponent(cc.Sprite).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame ){
+                if( fiveCount == 5 ){
+                    if( this.gameState === "black" ){
+                        this.overLabel.string = "你赢了";
+                        this.overSprite.node.x = 0;
+                    }else{
+                        this.overLabel.string = "你输了";
+                        this.overSprite.node.x = 0;
+                    }
+                    this.gameState = "over";
+                    return ;
+                }
+            }else{
+                fiveCount=0;
+            }
+        }
+
+        //没有输赢
+        if( this.gameState === "black" ){
+            this.gameState = "white";
+        }else{
+            this.gameState = "black";
+        }
+
     },
 
 
