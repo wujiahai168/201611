3e8e1fcff66a8c35f99d311f521e4ec007c8e32f
wuijahai168@126.com <wuijahai168@126.com>
2019-05-17 16:37:58 +0800

    school add other tilemap

--- TiledMapTest-20190516/assets/script/map.js
+++ TiledMapTest-20190516/assets/script/map.js
@@ -1,115 +1,16 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
     extends: cc.Component,
-
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-        
-        //player:cc.Node,
-
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad () {
-        this.player = this.node.getChildByName("player");
-        //cc.log( this.player );
-
+    // use this for initialization
+    onLoad: function () {
+        this.player = this.node.getChildByName('player');
         this.loadMap();
-
-        cc.systemEvent.on( cc.SystemEvent.EventType.KEY_DOWN,this.onKeyDown,this );
-        
-        // this.node.on( cc.Node.EventType.MOUSE_MOVE,function(event){
-        //     cc.log("mouse move" , event.getLocation() );
-        // },this );        
-
-        //cc.log(this.player);
-
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
     },
-
-    start () {
-
-    },
-
-    // update (dt) {},
-
-    loadMap(){
-        this.node.setPosition( cc.visibleRect.bottomLeft );
-        this.tiledMap = this.node.getComponent( cc.TiledMap );
-
-        let players = this.tiledMap.getObjectGroup( "players" );
-        let startPoint = players.getObject( "startPoint" );
-        let endPoint = players.getObject( "endPoint" );
-
-        let startPos = cc.v2( startPoint.offset.x , startPoint.offset.y );
-        let endPos = cc.v2( endPoint.offset.x, endPoint.offset.y );
-
-        this.barriers = this.tiledMap.getLayer( "barriers" );
-        this.stars = this.tiledMap.getLayer( "stars" );
-
-
-        
-        cc.log( "startPoint",startPoint );
-        cc.log( "endPoint",endPoint );
-        cc.log("startPos",startPos);
-        cc.log("endPos",endPos);
-
-        cc.log("-----------------------------------");
-
-        startPos = this.node.convertToNodeSpaceAR( startPos );
-
-        this.playerTile = this.startTile = this.getTilePos( startPos );
-        this.endTile = this.getTilePos( endPos );
-
-        cc.log( "startPoint",startPoint );
-        cc.log( "endPoint",endPoint );
-        cc.log("startPos",startPos);
-        cc.log("endPos",endPos);
-
-        // cc.log( "playerTile---",this.playerTile );
-        // cc.log( this.barriers );
-        // cc.log( this.stars );
-
-        this.updatePlayerPos();
-    },
-
-    getTilePos( posInPixel ){
-        let mapSize = this.node.getContentSize();
-        //cc.log("mapSizemapSizemapSizemapSizemapSizemapSize",mapSize);
-        let tileSize = this.tiledMap.getTileSize();
-        let x = Math.floor( posInPixel.x / tileSize.width );
-        let y = Math.floor( posInPixel.y / tileSize.height );
-        cc.log("mapSize--tileSize",x,y);
-        return cc.v2( x,y );
-    },
-
-    onKeyDown( event ){
-        var newTile = cc.v2( this.playerTile.x ,  this.playerTile.y );
-        //cc.log( "onKeyDown - newTile",newTile );
-        switch( event.keyCode ){
+    onKeyDown:function(event){
+        var newTile = cc.p(this.playerTile.x, this.playerTile.y);
+        switch(event.keyCode) {
             case cc.macro.KEY.up:
                 newTile.y -= 1;
                 break;
@@ -125,57 +26,68 @@ cc.Class({
             default:
                 return;
         }
-        // let v2 = this.node.convertToWorldSpaceAR( newTile );
-        // this.tryMoveToNewTile( v2 );
-        this.tryMoveToNewTile( newTile );
-        
+        this.tryMoveToNewTile(newTile);
     },
-
-    tryMoveToNewTile( newTile ){
-
-        cc.log( "tryMoveToNewTile - newTile",newTile );
+    //加载地图文件时调用
+    loadMap: function () {
+        //初始化地图位置
+        this.node.setPosition(cc.visibleRect.bottomLeft);
+        //地图
+        this.tiledMap = this.node.getComponent(cc.TiledMap);
+        //players对象层
+        let players = this.tiledMap.getObjectGroup('players');
+        //startPoint和endPoint对象
+        let startPoint = players.getObject('startPoint');
+        let endPoint = players.getObject('endPoint');
+        //像素坐标
+        let startPos = cc.p(startPoint.offset.x, startPoint.offset.y);
+        let endPos = cc.p(endPoint.offset.x, endPoint.offset.y);
+        //障碍物图层和星星图层
+        this.barriers = this.tiledMap.getLayer('barriers');
+        this.stars = this.tiledMap.getLayer('stars');
+        //出生Tile和结束Tile
+        this.playerTile = this.startTile = this.getTilePos(startPos);
+        this.endTile = this.getTilePos(endPos);
+        //更新player位置
+        this.updatePlayerPos();
+    },
+    tryMoveToNewTile: function(newTile) {
         let width = this.tiledMap.node.width;
         let height = this.tiledMap.node.height;
-        if( newTile.x < 0 || newTile.x >= width ) return;
-        if( newTile.y < 0 || newTile.y >= height ) return;
-
-        if( this.barriers.getTileGIDAt( newTile ) ){
-            cc.LoadingItems( "this way is blocked! " );
+        if (newTile.x < 0 || newTile.x >= width) return;
+        if (newTile.y < 0 || newTile.y >= height) return;
+        if (this.barriers.getTileGIDAt(newTile)) {//GID=0,则该Tile为空
+            cc.log('This way is blocked!');
             return false;
         }
-
-        this.tryCatchStar( newTile );
-
+        this.tryCatchStar(newTile);
         this.playerTile = newTile;
         this.updatePlayerPos();
-
-        //cc.log( "newTile - 中间 ---",newTile );
-        //cc.log( "endTile",this.endTile );
-
-        if( newTile.equals( this.endTile ) ){
-            cc.log("succeed");
+        
+        if ( newTile.equals(this.endTile) ) {
+            cc.log('succeed');
         }
-
-        // if( cc.Vec2.equls( this.playerTile , this.endTile ) ){
-        //     cc.log("succeed");
+        // if (cc.pointEqualToPoint(this.playerTile, this.endTile)) {
+        //     cc.log('succeed');
         // }
-
     },
-
-    tryCatchStar( newTile ){
-        let GID = this.stars.getTileGIDAt( newTile );
-        let prop = this.tiledMap.getPropertiesForGID( GID );
-
-        if( this.stars.getTileGIDAt( newTile ) ){
-            this.stars.removeTileAt( newTile );
+    tryCatchStar: function(newTile){
+        let GID = this.stars.getTileGIDAt(newTile);
+        let prop = this.tiledMap.getPropertiesForGID(GID);
+        if (this.stars.getTileGIDAt(newTile)) {//GID=0,则该Tile为空
+            this.stars.removeTileAt(newTile);
         }
-
-    }, 
-
-    updatePlayerPos(){
-        let pos = this.barriers.getPositionAt( this.playerTile );
-        this.player.setPosition( pos );
-        //cc.log( pos );
     },
-
+    //将像素坐标转化为瓦片坐标
+    getTilePos: function(posInPixel) {
+        let mapSize = this.node.getContentSize();
+        let tileSize = this.tiledMap.getTileSize();
+        let x = Math.floor(posInPixel.x / tileSize.width);
+        let y = Math.floor(posInPixel.y / tileSize.height);
+        return cc.p(x, y);
+    },
+    updatePlayerPos: function() {
+        let pos = this.barriers.getPositionAt(this.playerTile);
+        this.player.setPosition(pos);
+    },
 });
